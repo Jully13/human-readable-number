23a1e710851718e27c0ca579600b67fe7c82a6e3
Jully13 <7448778@mail.ru>
2023-01-15 00:17:46 +0300

    feat: writing a function for the task solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
-  
+
+let a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+'ten'];
+let b = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let c = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let result = '';
+
+if (number == 0) {return 'zero'};
+
+if (number <= 10) {return (a[number])};
+
+if (number < 20) {return (b[number-10])};
+
+if (number <100) {
+    result = (c[Number(number.toString()[0])] + ' ' + a[Number(number.toString()[1])])
+    return result.replace(/\s+/g, ' ').trim();
 }
+
+if (number <1000) {
+    let n = +(number.toString().slice(1));
+    if (n <= 10) {
+        result = ((a[Number(number.toString()[0])] + ' hundred ' + a[n]))}
+        else {
+            if (n < 20) {
+                result = ((a[Number(number.toString()[0])] + ' hundred ' + b[n-10]))}
+                else {
+                    result = ((a[Number(number.toString()[0])] + ' hundred ' + `${c[Number(n.toString()[0])]}` + ` ${a[Number(n.toString()[1])]}`))
+                 }}
+    return result.replace(/\s+/g, ' ').trim();
+
+}
+}
+
+
+
